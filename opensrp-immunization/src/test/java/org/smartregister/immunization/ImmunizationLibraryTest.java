0577f8f70849f76ad2fa6e1028f2d5f9b3690ebb
raihan-mpower <raihan@mpower-social.com>
2017-09-13 17:58:55 +0600

    RA|added test methods for immunization library class

---
+++ opensrp-immunization/src/test/java/org/smartregister/immunization/ImmunizationLibraryTest.java
@@ -0,0 +1,94 @@
+package org.smartregister.immunization;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.smartregister.Context;
+import org.smartregister.commonregistry.CommonFtsObject;
+import org.smartregister.immunization.service.intent.RecurringIntentService;
+import org.smartregister.immunization.view.ImmunizationRowCard;
+import org.smartregister.immunization.view.ImmunizationRowGroup;
+import org.smartregister.repository.Repository;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * Created by onaio on 30/08/2017.
+ */
+
+
+public class ImmunizationLibraryTest extends BaseUnitTest {
+
+    @Mock
+    private Repository repository;
+
+    @Mock
+    private CommonFtsObject commonFtsObject;
+
+    @Mock
+    private Context context;
+
+    @Mock
+    private ImmunizationLibrary immunizationLibrary;
+
+    @Before
+    public void setUp() {
+        repository = Mockito.mock(Repository.class);
+        context = Mockito.mock(Context.class);
+        commonFtsObject = Mockito.mock(CommonFtsObject.class);
+        ImmunizationLibrary.init(context,repository,commonFtsObject);
+        immunizationLibrary = ImmunizationLibrary.getInstance();
+        initMocks(this);
+    }
+
+    @Test
+    public void assertDefaultConstructorsCreateNonNullObjectOnInstantiation() {
+        assertNotNull(ImmunizationLibrary.getInstance());
+    }
+
+    @Test
+    public void assertRepositoryReturnsNonNUllObject() {
+        assertNotNull(ImmunizationLibrary.getInstance().getRepository());
+    }
+
+    @Test
+    public void assertRecurringServiceRecordRepositoryReturnsNonNUllObject() {
+        assertNotNull(ImmunizationLibrary.getInstance().recurringServiceRecordRepository());
+    }
+
+    @Test
+    public void assertRecurringServiceTypeRepositoryReturnsNonNUllObject() {
+        assertNotNull(ImmunizationLibrary.getInstance().recurringServiceTypeRepository());
+    }
+
+    @Test
+    public void assertEventClientRepositoryReturnsNonNUllObject() {
+        assertNotNull(ImmunizationLibrary.getInstance().eventClientRepository());
+    }
+
+    @Test
+    public void assertVaccineRepositoryReturnsNonNUllObject() {
+        assertNotNull(ImmunizationLibrary.getInstance().vaccineRepository());
+    }
+
+    @Test
+    public void assertVaccineNameRepositoryReturnsNonNUllObject() {
+        assertNotNull(ImmunizationLibrary.getInstance().vaccineNameRepository());
+    }
+
+    @Test
+    public void assertVaccineTypeRepositoryReturnsNonNUllObject() {
+        assertNotNull(ImmunizationLibrary.getInstance().vaccineTypeRepository());
+    }
+
+    @Test
+    public void assertContextReturnsNonNUllObject() {
+        assertNotNull(ImmunizationLibrary.getInstance().context());
+    }
+
+}
