8ca01b559a414a9f686ddef19d4616e190cdd284
Ephraim Kigamba <ekigamba@ona.io>
2018-04-26 16:01:18 +0300

    Add a listener for notifying when all vaccine wrappers have been loaded
    Also changed the library version to 1.2.12-SNAPSHOT

---
+++ opensrp-immunization/src/main/java/org/smartregister/immunization/listener/VaccineCardAdapterLoadingListener.java
@@ -0,0 +1,10 @@
+package org.smartregister.immunization.listener;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 26/04/2018.
+ */
+
+public interface VaccineCardAdapterLoadingListener {
+
+    void onFinishedLoadingVaccineWrappers();
+}
