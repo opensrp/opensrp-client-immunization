67a67e8c0caa4bc092dd32dbd2854d2c4ccd094a
Manu Tarus <manutarus@gmail.com>
2019-03-14 21:21:57 +0300

    add recurring service job

--- opensrp-immunization/src/main/java/org/smartregister/immunization/job/RecurringServiceJob.java
+++ opensrp-immunization/src/main/java/org/smartregister/immunization/job/RecurringServiceJob.java
@@ -4,17 +4,17 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import org.smartregister.AllConstants;
-import org.smartregister.immunization.service.intent.VaccineIntentService;
+import org.smartregister.immunization.service.intent.RecurringIntentService;
 import org.smartregister.job.BaseJob;
 
-public class VaccineService3Job extends BaseJob {
+public class RecurringServiceJob extends BaseJob {
 
-    public static final String TAG = "VaccineServiceJob";
+    public static final String TAG = "RecurringServiceJob";
 
     @NonNull
     @Override
     protected Result onRunJob(@NonNull Params params) {
-        Intent intent = new Intent(getApplicationContext(), VaccineIntentService.class);
+        Intent intent = new Intent(getApplicationContext(), RecurringIntentService.class);
         getApplicationContext().startService(intent);
         return params != null && params.getExtras().getBoolean(AllConstants.INTENT_KEY.TO_RESCHEDULE, false) ? Result.RESCHEDULE : Result.SUCCESS;
     }
