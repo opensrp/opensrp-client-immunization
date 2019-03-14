5f0ffaa349e69d14c3baf6751c55cfc8250fc413
Manu Tarus <manutarus@gmail.com>
2019-03-14 21:16:53 +0300

    Add new vaccines

---
+++ opensrp-immunization/src/main/java/org/smartregister/immunization/job/RecurringServiceJob.java
@@ -0,0 +1,21 @@
+package org.smartregister.immunization.job;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import org.smartregister.AllConstants;
+import org.smartregister.immunization.service.intent.VaccineIntentService;
+import org.smartregister.job.BaseJob;
+
+public class VaccineService3Job extends BaseJob {
+
+    public static final String TAG = "VaccineServiceJob";
+
+    @NonNull
+    @Override
+    protected Result onRunJob(@NonNull Params params) {
+        Intent intent = new Intent(getApplicationContext(), VaccineIntentService.class);
+        getApplicationContext().startService(intent);
+        return params != null && params.getExtras().getBoolean(AllConstants.INTENT_KEY.TO_RESCHEDULE, false) ? Result.RESCHEDULE : Result.SUCCESS;
+    }
+}
---
+++ opensrp-immunization/src/main/java/org/smartregister/immunization/job/VaccineServiceJob.java
@@ -0,0 +1,21 @@
+package org.smartregister.immunization.job;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import org.smartregister.AllConstants;
+import org.smartregister.job.BaseJob;
+import org.smartregister.sync.intent.CampaignIntentService;
+
+public class CampaignServiceJob extends BaseJob {
+
+    public static final String TAG = "CampaignServiceJob";
+
+    @NonNull
+    @Override
+    protected Result onRunJob(@NonNull Params params) {
+        Intent intent = new Intent(getApplicationContext(), CampaignIntentService.class);
+        getApplicationContext().startService(intent);
+        return params != null && params.getExtras().getBoolean(AllConstants.INTENT_KEY.TO_RESCHEDULE, false) ? Result.RESCHEDULE : Result.SUCCESS;
+    }
+}
