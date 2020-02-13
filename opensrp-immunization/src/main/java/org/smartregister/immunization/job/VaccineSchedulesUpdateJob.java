056f3838ae5200edb4e9ae4286f8d8e3b956331e
Ephraim Kigamba <ekigamba@ona.io>
2020-02-13 14:34:59 +0300

    Make cmmon table name configurable in job

--- opensrp-immunization/src/main/java/org/smartregister/immunization/job/VaccineSchedulesUpdateJob.java
+++ opensrp-immunization/src/main/java/org/smartregister/immunization/job/VaccineSchedulesUpdateJob.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import org.smartregister.AllConstants;
+import org.smartregister.immunization.service.intent.VaccineSchedulesUpdateIntentService;
 import org.smartregister.immunization.util.IMConstants;
 import org.smartregister.job.BaseJob;
 
@@ -13,14 +14,19 @@ import org.smartregister.job.BaseJob;
  * Created by Ephraim Kigamba - ekigamba@ona.io on 2020-02-13
  */
 
-public class VaccineSchedulesUpdateJob extends BaseJob {
+public abstract class VaccineSchedulesUpdateJob extends BaseJob {
 
     @NonNull
     @Override
     protected Result onRunJob(@NonNull Params params) {
-        Intent intent = new Intent(getApplicationContext(), IMConstants.IntentKey.VaccineScheduleUpdateIntentService.class);
+        Intent intent = new Intent(getApplicationContext(), VaccineSchedulesUpdateIntentService.class);
+        intent.putExtra(IMConstants.IntentKey.VaccineScheduleUpdateIntentService.CLIENT_TABLE_NAME, getClientTableName());
+
         getApplicationContext().startService(intent);
         return params != null && params.getExtras().getBoolean(AllConstants.INTENT_KEY.TO_RESCHEDULE, false)
                 ? Result.RESCHEDULE: Result.SUCCESS;
     }
+
+    @NonNull
+    protected abstract String getClientTableName();
 }
