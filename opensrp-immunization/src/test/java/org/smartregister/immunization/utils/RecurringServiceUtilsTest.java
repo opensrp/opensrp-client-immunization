8b18570ab62f796210ccab09f8304f68e24f84be
Allan O <a.onchuru@gmail.com>
2020-09-17 17:06:56 +0300

    :white_check_mark: Test can save recurring service

--- opensrp-immunization/src/test/java/org/smartregister/immunization/utils/RecurringServiceUtilsTest.java
+++ opensrp-immunization/src/test/java/org/smartregister/immunization/utils/RecurringServiceUtilsTest.java
@@ -1,10 +1,23 @@
 package org.smartregister.immunization.utils;
 
+import org.joda.time.DateTime;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.RuntimeEnvironment;
 import org.smartregister.immunization.BaseUnitTest;
+import org.smartregister.immunization.ImmunizationLibrary;
 import org.smartregister.immunization.R;
+import org.smartregister.immunization.domain.ServiceRecord;
+import org.smartregister.immunization.domain.ServiceWrapper;
+import org.smartregister.immunization.repository.RecurringServiceRecordRepository;
 import org.smartregister.immunization.util.RecurringServiceUtils;
 import org.smartregister.immunization.view.ServiceGroup;
 
@@ -14,9 +27,24 @@ import java.util.List;
 /**
  * Created by ndegwamartin on 2020-03-23.
  */
+
+@PrepareForTest({ImmunizationLibrary.class})
 public class RecurringServiceUtilsTest extends BaseUnitTest {
 
     private static final String SG_BETA = "SG Beta";
+
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+
+    @Mock
+    private RecurringServiceRecordRepository recurringServiceRecordRepository;
+    @Mock
+    private ImmunizationLibrary immunizationLibrary;
+
+    @Before
+    public void setUp() {
+        org.mockito.MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void testGetLastOpenedServiceViewReturnsCorrectView() {
@@ -44,6 +72,20 @@ public class RecurringServiceUtilsTest extends BaseUnitTest {
 
         Assert.assertEquals(SG_BETA, lastOpenedServiceView.getTag(R.id.key));
 
+    }
+
+    @Test
+    public void testCanSaveService() {
+        PowerMockito.mockStatic(ImmunizationLibrary.class);
+        PowerMockito.when(ImmunizationLibrary.getInstance()).thenReturn(immunizationLibrary);
+        PowerMockito.when(immunizationLibrary.recurringServiceRecordRepository()).thenReturn(recurringServiceRecordRepository);
+        ServiceWrapper tag = new ServiceWrapper();
+        tag.setUpdatedVaccineDate(new DateTime(), true);
+        String baseEntityId = "test-entity-id";
+        String providerId = "testProvider";
+        String locationId = "test-location-id";
+        RecurringServiceUtils.saveService(tag, baseEntityId, providerId, locationId);
+        Mockito.verify(recurringServiceRecordRepository, Mockito.times(1)).add((ServiceRecord) ArgumentMatchers.any());
     }
 }
 
